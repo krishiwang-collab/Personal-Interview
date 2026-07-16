a8532ec376d56d8d282722f758b332209d3aa21b
Kris <kris.hi.wang@gmail.com>
2026-07-16 09:30:00 +0800

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,11 @@ function App() {
     <LanguageProvider>
       <Layout>
         <Routes>
+          <Route path="/Personal-Interview/" element={<Home />} />
+          <Route path="/Personal-Interview/about" element={<About />} />
+          <Route path="/Personal-Interview/experience" element={<Experience />} />
+          <Route path="/Personal-Interview/certificates" element={<Certificates />} />
+          <Route path="/Personal-Interview/contact" element={<Contact />} />
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/experience" element={<Experience />} />
